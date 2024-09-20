bb54b839fa1f892ea232823e6af4f9df59c4c7fd
350313043 <350313043@C-MXL2393F2L.tdsb.on.ca>
2024-09-20 11:11:22 -0400

    Adding ICS3U project with HelloWorld class

---
+++ ICS3U-PefanisD/src/unit1/HelloWorld.java
@@ -0,0 +1,21 @@
+package unit1;
+/**
+ * Description of the program: This program prints Hello World,
+ * a second statement. <br>
+ * Date: Sept. 19, 2024
+ * @author Daniella Pefanis
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//This prints hello to the system console
+		System.out.println("Hello World!");
+		System.out.println("My name is Daniella");
+
+		/*
+		 * This is a block comment.
+		 * You can add notes on multiple lines. 
+		 */
+	}
+
+}
