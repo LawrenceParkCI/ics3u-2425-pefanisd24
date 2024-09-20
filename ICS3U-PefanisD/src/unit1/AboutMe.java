b203723247c24aa5568e13d3916a35845b66fd20
350313043 <350313043@C-MXL2393F2L.tdsb.on.ca>
2024-09-20 11:41:55 -0400

    This is a few tings about me.

---
+++ ICS3U-PefanisD/src/unit1/AboutMe.java
@@ -0,0 +1,20 @@
+package unit1;
+/**
+ * Description: This program describes who I am. It includes my name, my grade, my teacher's name and what school I go to.
+ * Date: September 20. 2024
+ * @author: Daniella Pefanis
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// This prints hello to the system console
+		
+		System.out.println("About Me");
+		System.out.println("--------");
+		System.out.print("My name is Daniella Pefanis,");
+		System.out.println(" and I am in grade 11.");
+		System.out.println("My teacher is Ms. Kemp.");
+		System.out.println("I go to Lawrence Park.");
+		System.out.println("I used to go to Loretto Abbey, but transfered last year.");
+	}
+}
--- ICS3U-PefanisD/src/unit1/HelloWorld.java
+++ ICS3U-PefanisD/src/unit1/HelloWorld.java
@@ -1,12 +1,6 @@
 package unit1;
-/**
- * Description of the program: This program prints Hello World,
- * a second statement. <br>
- * Date: Sept. 19, 2024
- * @author Daniella Pefanis
- */
-public class HelloWorld {
 
+public class HelloWorld {
 	public static void main(String[] args) {
 		//This prints hello to the system console
 		System.out.println("Hello World!");
