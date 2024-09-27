dd61a157b666a35f949e4f3fcad3cedd84f2601d
350313043 <350313043@C-MXL2393F2L.tdsb.on.ca>
2024-09-27 10:12:55 -0400

    Finished

--- ICS3U-PefanisD/src/unit1/HelloWorld.java
+++ ICS3U-PefanisD/src/unit1/HelloWorld.java
@@ -1,5 +1,11 @@
 package unit1;
 
+/**
+ * Description: This program says my name.
+ * Date: September 20. 2024
+ * @author: Daniella Pefanis
+ */
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		//This prints hello to the system console
