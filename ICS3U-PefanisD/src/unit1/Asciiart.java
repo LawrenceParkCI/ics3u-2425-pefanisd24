299dc58b8279a305abc5d7a785fae103a1484a79
350313043 <350313043@C-MXL2393F2L.tdsb.on.ca>
2024-09-27 10:06:27 -0400

    Finished the program

---
+++ ICS3U-PefanisD/src/unit1/Asciiart.java
@@ -0,0 +1,66 @@
+package unit1;
+
+public class Asciiart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("              _                                                             _                                                ");
+		System.out.println("            /' `\\                                    /' /'                /' `\\                                              ");
+		System.out.println("          /'     )                                 /' /'                /'     )            /')                              ");
+		System.out.println("        /'      /'____     ,____     O  ____     /' /' ____           /' (___,/'____      /' /' ____     ,____     O  ____   ");
+		System.out.println("      /'      /'/'    )   /'    )  /' /'    )  /' /' /'    )        /'        /'    )  -/'--' /'    )   /'    )  /' /'    )--");
+		System.out.println("    /'      /'/'    /'  /'    /' /' /(___,/' /' /' /'    /'       /'        /(___,/'  /'    /'    /'  /'    /' /'  '---,     ");
+		System.out.println("(,/' (___,/' (___,/(__/'    /(__(__(________(__(__(___,/(__   (,/'         (________/(_____(___,/(__/'    /(__(__(___,/      ");
+		System.out.println("                                                                                  /'                                         ");
+		System.out.println("                                                                                /'                                           ");
+		System.out.println("                                                                              /'                                             ");
+		System.out.println("   @@@@                  @@@  ");
+		System.out.println("  @@@@@@@@@@@@@@@@@@@@@@@@@@@                                 ");
+		System.out.println(" @@@   @@            @@@  @@@@                                 ");
+		System.out.println("@@@@                       @@@                             ");
+		System.out.println("@@@  @@@@@@@@    @@@@@@@@   @@@                                ");
+		System.out.println("@@@ @@@@@@@@@    @@@@@@@@@  @@@                                ");
+		System.out.println("@@@ @@@@@@@@@@  @@@@@@@@@@  @@@                                ");
+		System.out.println("@@@ @@@@@@@@@@ @@@@@@@@@@   @@@                                ");
+		System.out.println("@@@   @@@@@ @@@@@@ @@@@     @@@                                ");
+		System.out.println("@@@          @@@@           @@                                 ");
+		System.out.println(" @@@         @@@@          @@@                                 ");
+		System.out.println(" @@@@         @@         @@@@@@                                ");
+		System.out.println(" @@@@@@                 @@@@@@@@@@@                            ");
+		System.out.println("  @@@@@@@@           @@@@@@@@@@@@@@@@@                         ");
+		System.out.println("   @@@  @@@@@@@@@@@@@@  @@@@      @@@@@@                       ");
+		System.out.println("    @@@     @@@@@@     @@@@          @@@@@@                    ");
+		System.out.println("    @@@                @@@@             @@@@@                  ");
+		System.out.println("  @@@@@                 @@@@                   @@@@@@                   ");
+		System.out.println("    @@@@                 @@@@                     @@@@@                  ");
+		System.out.println("      @@@@                @@@@                      @@@@@@                ");
+		System.out.println("        @@@@               @@@@@                       @@@@@               ");
+		System.out.println("         @@@@@              @@@@                         @@@@@             ");
+		System.out.println("          @@@@@             @@@@@                         @@@@@            ");
+		System.out.println("            @@@@             @@@@@@                         @@@@           ");
+		System.out.println("             @@@@@             @@@@@                         @@@@@         ");
+		System.out.println("              @@@@@             @@@@@@                        @@@@@        ");
+		System.out.println("               @@@@@@             @@@@@@                       @@@@@       ");
+		System.out.println("                 @@@@@@             @@@@@@                      @@@@@      ");
+		System.out.println("                   @@@@@              @@@@@@                     @@@@      ");
+		System.out.println("                   @@@@@@@@              @@@@@@                  @@@@@     ");
+		System.out.println("                   @@@@@@@@@@              @@@@@@                 @@@@@    ");
+		System.out.println("                   @@@@  @@@@@@              @@@@@@                @@@@@   ");
+		System.out.println("                   @@@     @@@@@@               @@@@@@              @@@@   ");
+		System.out.println("                   @@@       @@@@         @@       @@@@@@           @@@@@  ");
+		System.out.println("                  @@@@       @@@@@        @@@@       @@@@@@@         @@@@@ ");
+		System.out.println("                  @@@       @@@@@@@       @@@@@@@          @@@@@@@    @@@@@");
+		System.out.println("                   @@@@@@@@@@@@ @@@@@@@@@@@@@@  @@@@@           @@@@@@@@@@@");
+		System.out.println("                    @@@@@@@@@@      @@@@@         @@@@@          @@@@@@@@@@");
+		System.out.println("                   @@@@@@          @@@@@           @@@@@@        @@@@@ @@@@");
+		System.out.println("                  @@@@@@          @@@@@@             @@@@@@@@@@@@@@@@@     ");
+		System.out.println("                 @@@@@@          @@@@@@               @@@@@@@@@@@@@@@      ");
+		System.out.println("                @@@@@@          @@@@@@                                     ");
+		System.out.println("               @@@@@@           @@@@@                                      ");
+		System.out.println(" @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                            ");
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                            ");
+
+
+	}
+
+}
---
+++ ICS3U-PefanisD/src/unit1/Bingo.java
@@ -0,0 +1,29 @@
+package unit1;
+
+public class Bingo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("How to play BINGO!!!!");
+		System.out.println("");
+		System.out.println("Step 1: Be prepared to have fun and test your luck!");
+		System.out.println("Step 2: The caller randomly pulls a numbered bingo ball.");
+		System.out.println("Step 3: The number is placed on the bingo card and called out.");
+		System.out.println("Step 4: Players look for that called number on their bingo card.");
+		System.out.println("Step 5: If the player has that number, it's marked off.");
+		System.out.println("Step 6: Steps 2-5 are repeated until a player matches the BINGO pattern.");
+		System.out.println("Step 7: The winning player must shout BINGO! to officially win the game.");
+		System.out.println("-------------------------------------------------------------------------");
+		System.out.println("BINGO Game Card Example:");
+		System.out.println("");
+		System.out.println("---------------------------------------------------");
+		System.out.format("%10s%10s%10s%10s%10s", "B","I","N","G","O");
+		System.out.format("\n%10s%10s%10s%10s%10s", "8","23","34","57","72");
+		System.out.format("\n%10s%10s%10s%10s%10s", "3","29","43","60","64");
+		System.out.format("\n%10s%10s%10s%10s%10s", "10","17","FREE","48","70");
+		System.out.format("\n%10s%10s%10s%10s%10s", "1","30","45","59","75");
+		System.out.format("\n%10s%10s%10s%10s%10s", "2","15","33","46","61");
+		System.out.format("\n---------------------------------------------------");
+		System.out.println("\nHAVE FUN!!");
+	}
+
+}
