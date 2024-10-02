3e8c70c4c169baa385bdc0745dcf5964c62a89c8
350313043 <350313043@C-MXL2393F2L.tdsb.on.ca>
2024-10-02 11:24:18 -0400

    Finished String Variables Worksheet

--- ICS3U-PefanisD/src/unit1/MakeLabels.java
+++ ICS3U-PefanisD/src/unit1/MakeLabels.java
@@ -7,10 +7,19 @@ public class MakeLabels {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		
+		String subject;
+		String name;
+		System.out.println("Type in the subject and press <Enter>");
+		subject = sc.nextLine();
+		
+		System.out.println("Type in the name and press <Enter>");
+		name= sc.nextLine();
+		
 		System.out.println();
 		System.out.println("*******************************");
-		System.out.println("John");
-		System.out.println("Computers");
+		System.out.println(name);
+		System.out.println(subject);
 		System.out.println("********************************");
 		sc.close();
 	}
