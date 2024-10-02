7c5cfd9a0669983d3053555635e8fab97f56759d
350313043 <350313043@C-MXL2393F2L.tdsb.on.ca>
2024-10-02 10:56:07 -0400

    Part A.1

---
+++ ICS3U-PefanisD/src/unit1/MakeLabels.java
@@ -0,0 +1,18 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class MakeLabels {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		System.out.println("*******************************");
+		System.out.println("John");
+		System.out.println("Computers");
+		System.out.println("********************************");
+		sc.close();
+	}
+
+}
--- ICS3U-PefanisD/src/unit1/MathOperators.java
+++ ICS3U-PefanisD/src/unit1/MathOperators.java
@@ -16,15 +16,15 @@ public class MathOperators {
 		 * Pre: Kind of Math, but not really What do you notice is happening?
 		 */
 
-		System.out.println("Butter" + "fly");
+//		System.out.println("Butter" + "fly");
 
-		System.out.println("1 + 2 + 3 + 4 + 5");
+//		System.out.println("1 + 2 + 3 + 4 + 5");
 
-		System.out.println(1 + 2 + 3 + 4 + 5);
+//		System.out.println(1 + 2 + 3 + 4 + 5);
 
-		System.out.println(1 + 2 + "3 + 4 + 5");
+//		System.out.println(1 + 2 + "3 + 4 + 5");
 
-		System.out.println("1 + 2 + 3" + 4 + 5);
+//		System.out.println("1 + 2 + 3" + 4 + 5);
 
 		// Why do you think the last two outputs act so differently?
 		// Because there are quotations around different numbers
@@ -38,33 +38,29 @@ public class MathOperators {
 		 */
 
 		// + means: Addition
-		System.out.println(1+3);
+//		System.out.println(1+3);
 
 		// - means: Subtraction
-		System.out.println(5-3);
+//		System.out.println(5-3);
 
 		// * means: Multiplication
-		System.out.println(5*2);
+//		System.out.println(5*2);
 
 		// / means: Division
-		System.out.println(10/2);
+//		System.out.println(10/2);
 
 		// Print the following expression: (3 + 2) * 5
-		System.out.println((3+2) *5);
+//		System.out.println((3+2) *5);
 		// Print the following expression: 3 + 2 * 5
-		System.out.println(3+2*5);
+//		System.out.println(3+2*5);
 
 		// Was this expected? Why or why not?
 		//Yes, because of Bedmas.
 
 		//*Part 2 - Calculate Using Operators Print out, using one println statement,
 		//*what 45 degrees Celsius is in Fahrenheit
-		System.out.println(F=(9/5)45+32);
-		* 
-		// * Print out, using a print and a println statement, what 900 Fahrenheit is in
-		// * Celsius
-		*/
-
+//		System.out.println(F=(9/5)45+32);
+	
 		/*
 		 * Part 3 Figure out what is the meaning of % as a math operator. Use print
 		 * statements to check your answer.
