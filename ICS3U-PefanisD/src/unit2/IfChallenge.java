3bc6e8fd5223df73a90cbe7b7c6dcc8ea231a693
350313043 <350313043@C-MXL2393F2L.tdsb.on.ca>
2024-11-05 10:15:36 -0500

    Finished

--- ICS3U-PefanisD/src/unit2/IfChallenge.java
+++ ICS3U-PefanisD/src/unit2/IfChallenge.java
@@ -3,48 +3,48 @@ package unit2;
 import java.util.Scanner;
 
 /**
-* Description: This code prints a certain phrase depending on the number
-* Date: November 5, 2024
-* @author Daniella Pefanis
-*/
+ * Description: This code prints a certain phrase depending on the number
+ * Date: November 5, 2024
+ * @author Daniella Pefanis
+ */
 
 public class IfChallenge {
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 
-    
-	System.out.println("What is your favourite number?");
-	int favNumber = in.nextInt();
-	
-	if (favNumber<0) {
-		   System.out.println("You need to look on the bright side!");
-    }
-	
-	if (favNumber % 2 != 0) {
-	      System.out.println("That's a bit strange!");
-	    }
-	
-	  if (favNumber % 3 == 0 && favNumber % 2 == 0) {
-	      System.out.println("You should really consider thinking about 6.");
-	    } else {
-	      System.out.println("You can keep wholesome.");
-	    }
-	  
-	  int onesDigit = favNumber % 10;
-	    if (onesDigit == 8) {
-	      System.out.println("I too am full!");
-	    } else if (onesDigit == 9) {
-	      System.out.println("Canada is a nice country.");
-	    } else {
-	      System.out.println("Don't count on it.");
-	   
-	    }
-	    
-  	}
+
+		System.out.println("What is your favourite number?");
+		int favNumber = in.nextInt();
+
+		if (favNumber<0) {
+			System.out.println("You need to look on the bright side!");
+		}
+
+		if (favNumber % 2 != 0) {
+			System.out.println("That's a bit strange!");
+		}
+
+		if (favNumber % 3 == 0 && favNumber % 2 == 0) {
+			System.out.println("You should really consider thinking about 6.");
+		} else {
+			System.out.println("You can keep wholesome.");
+		}
+
+		int onesDigit = favNumber % 10;
+		if (onesDigit == 8) {
+			System.out.println("I too am full!");
+		} else if (onesDigit == 9) {
+			System.out.println("Canada is a nice country.");
+		} else {
+			System.out.println("Don't count on it.");
+
+		}
+
+	}
 }
 
-	
-    /*
+
+/*
 
     (IF)
 
@@ -66,4 +66,4 @@ public class IfChallenge {
     if the ones digit is 9, say Canada is a nice country, 
     and any other number should result in "don't count on 
     it". 
-    */
+ */
---
+++ ICS3U-PefanisD/src/unit2/IfChallenge2.java
@@ -0,0 +1,41 @@
+package unit2;
+
+import java.util.Scanner;
+
+/**
+ * Description: This code tells you if the numbers are in order or not.
+ * Date: November 5, 2024
+ * @author Daniella Pefanis
+ */
+
+public class IfChallenge2 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter the first number:");
+		int num1 = in.nextInt();
+
+		System.out.println("Enter the second number:");
+		int num2 = in.nextInt();
+
+		System.out.println("Enter the third number:");
+		int num3 = in.nextInt();
+
+
+		if (num1 < num2 && num2 < num3) {
+			System.out.println("The numbers are strictly increasing.");
+		} else if (num1 > num2 && num2 > num3) {
+			System.out.println("The numbers are strictly decreasing.");
+		} else {
+			System.out.println("The numbers are not strictly in order.");
+		}
+	}
+
+	//This is for the portfolio
+
+	/*Create a program that asks for three numbers.
+    Tell the user if the numbers are strictly in order.
+    ie.  2 5 11 or 5 6 7 are strictly in order.
+    ie.  6 5 7 or 5 5 7 are not
+	 */
+}
