3417cb193e7c0524fec6cfee992d7b31dc005bc4
350313043 <350313043@C-MXL2393F2L.tdsb.on.ca>
2024-11-19 09:11:57 -0500

    Finished assignment.

---
+++ ICS3U-PefanisD/src/unit2/Randomness.java
@@ -0,0 +1,27 @@
+package unit2;
+
+/**
+
+https://www.geeksforgeeks.org/java-math-random-method-examples/
+
+Description: This code prints a random number between 100-1000
+Date: November 19, 2024
+@author: Daniella Pefanis
+*/
+
+
+public class Randomness {
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      System.out.println(Math.random()*900+100);
+    }
+
+    //Run the program a few times. What do you think is the 
+    //range of the numbers generated?
+    //I think the range is 0-1
+
+    //Read the demo program. Write the code so that the program 
+    //will produce a random number between 100 and 1000.
+    
+  }
+}
