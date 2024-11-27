3a4c84250b9852b4b1efaf265a4dee57f2f3d152
350313043 <350313043@C-MXL2393F2L.tdsb.on.ca>
2024-11-27 10:22:31 -0500

    Finished

---
+++ ICS3U-PefanisD/src/unit2/StringLengthAndChars.java
@@ -0,0 +1,51 @@
+package unit2;
+import java.util.Scanner;
+
+/**
+Description:
+Date:
+@author 
+ */
+
+
+public class StringLengthAndChars {
+
+	/**
+	 * @param args unused
+	 */
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		/*
+      Goal: Using string methods with a for loop
+        .length()
+        .charAt(<position>)
+		 */
+
+		String name = "Ms. Kemp";
+
+		//write comments for the following code - what does it print out, and why?
+		System.out.println(name.length());   // 1. Prints the length of the string 'name'. It prints out = 8
+		System.out.println(name.charAt(1));   // 2. Prints the character at position 1 in the string 'name' It prints out = s
+		System.out.println(name.charAt(2)); // 3. Prints the character at position 2 in the string 'name'. It prints out = .
+		System.out.println(name.charAt(3)); // 4. Prints the character at position 3 in the string 'name'. It prints out = (space)
+
+		//.length() gets the total number of characters in the string.
+		//.charAt(position) gets the character at the specified position (index).
+		//counting starts at 0, which means the first character is at position 0.
+
+		/**
+      Ask for user input for name. Print out each individual letter of the name on separate lines, using a for loop
+		 */
+
+		System.out.print("Enter your name: ");
+		String userName = scanner.nextLine();
+
+		for (int i = 0; i < userName.length(); i++) {
+			System.out.println(userName.charAt(i));
+		}
+
+		scanner.close();
+
+	}
+}
