3d478cbeaf394e3ad7b1b2b33801c9c797bbbae8
350313043 <350313043@C-MXL2393F2L.tdsb.on.ca>
2024-12-17 09:31:47 -0500

    Finished this worksheet

---
+++ ICS3U-PefanisD/src/unit3/MathPlus.java
@@ -0,0 +1,31 @@
+package unit3;
+/**
+ * Description: This code uses a bunch of methods
+ * Date: December 17, 2024
+ * author @Daniella Pefanis
+ */
+
+public class MathPlus {
+
+	public static void distance(String[] args) {
+		// TODO Auto-generated method stub
+		// Returns the distance between two points (double)
+		
+	}
+
+	public static void hypotenuse(String[] args) {
+		// TODO Auto-generated method stub
+		//Returns the hypotenuse of a triangle given the length of the other two sides (double)
+	}
+
+	public static void numOfFactors(String[] args) {
+		// TODO Auto-generated method stub
+		//Returns the number of factors of the given number (int)
+	}
+
+	public static void isPrime(String[] args) {
+		// TODO Auto-generated method stub
+		// Returns true if the number given is prime, and false otherwise (boolean)
+
+	}
+}
