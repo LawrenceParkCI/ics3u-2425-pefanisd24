73f20ce489de336caddeb3301f481c7c7fdfa480
350313043 <350313043@C-MXL2393F2L.tdsb.on.ca>
2025-01-15 10:57:13 -0500

    Finished Assignment.

--- ICS3U-PefanisD/src/FinalProjectGrade11/FinalProject.java
+++ ICS3U-PefanisD/src/FinalProjectGrade11/FinalProject.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 
+import hsa_new.Console;
+
 
 /**
  * Description: This program predicts weather based on information the user inputs.
@@ -22,15 +24,25 @@ public class FinalProject {
 	 * @throws InterruptedException 
 	 */
 
-	
+	//Images
+	static Console c = new Console ();
 	static BufferedImage wizard = null;
 	static BufferedImage weathertypes = null;
-	
-	
+	static BufferedImage clock = null;
+	static BufferedImage window = null;
+	static BufferedImage thermometer = null;
+	static BufferedImage timeofday = null;
+	static BufferedImage veryhot = null;
+	static BufferedImage sunnycloud = null;
+	static BufferedImage clouds = null;
+	static BufferedImage rain = null;
+	static BufferedImage snowman = null;
+	static BufferedImage muddy = null;
 
 
-		
-	
+
+
+
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		// Starting point of the program
@@ -46,16 +58,74 @@ public class FinalProject {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		try {
+			clock = ImageIO.read(new File ("src/FinalProjectGrade11/clock.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			window = ImageIO.read(new File ("src/FinalProjectGrade11/window.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			thermometer = ImageIO.read(new File ("src/FinalProjectGrade11/thermometer.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			timeofday = ImageIO.read(new File ("src/FinalProjectGrade11/timeofday.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			veryhot = ImageIO.read(new File ("src/FinalProjectGrade11/very hot.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			sunnycloud = ImageIO.read(new File ("src/FinalProjectGrade11/sunnycloud.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			clouds = ImageIO.read(new File ("src/FinalProjectGrade11/clouds.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			rain = ImageIO.read(new File ("src/FinalProjectGrade11/rain.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			snowman = ImageIO.read(new File ("src/FinalProjectGrade11/snowman.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			muddy = ImageIO.read(new File ("src/FinalProjectGrade11/muddy.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		displayMenu();
 
 
 
 	}
-
+	//Setting the variables
 	private static void displayMenu() throws InterruptedException {
 		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
 
 		String weather = getUserInputWeather();
 		int temperature = getUserInputTemperature();
@@ -66,97 +136,106 @@ public class FinalProject {
 	}
 
 	public static String getUserInputWeather() throws InterruptedException {
-
-		System.out.println("Hey there!");
+		//Introduction of code
+		c.println("Hey there!");
 		Thread.sleep(2000);
-		drawImage (wizard,  0, 0, 700, 500, null);
-		System.out.println("I'm the Weather Wizard...");
+		c.drawImage (wizard,  0, 0, 700, 500, null);
+		c.println("I'm the Weather Wizard...");
 		Thread.sleep(2000);
-		System.out.println("You're favourite weather predictor!");
+		c.println("You're favourite weather predictor!");
 		Thread.sleep(2000);
-		System.out.println("No matter the weather...");
+		c.clear();
+		c.drawImage (weathertypes,  0, 0, 700, 500, null);
+		c.println("No matter the weather...");
 		Thread.sleep(2000);
-		System.out.println("No matter the time...");
+		c.clear();
+		c.drawImage (clock,  0, 0, 700, 500, null);
+		c.println("No matter the time...");
 		Thread.sleep(2000);
-		System.out.println("I've got your back!");
+		c.println("I've got your back!");
 		Thread.sleep(2000);
-		System.out.println("I do need your help though.");
+		c.println("I do need your help though.");
 		Thread.sleep(2000);
-		System.out.println("Only a few questions, don't worry ;)");
+		c.println("Only a few questions, don't worry ;)");
 		Thread.sleep(2000);
-		System.out.println("Let's Start!");
-		System.out.println(" ");
+		c.println("Let's Start!");
+		c.println(" ");
 		Thread.sleep(2000);
 
 
 		// Asks the user to input the current weather condition
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.println("Question 1:");
-		System.out.println("-----------");
+		c.println("Question 1:");
+		c.println("-----------");
 		Thread.sleep(2000);
-		System.out.println("Look Outside!");
+		c.clear();
+		c.drawImage (window,  0, 0, 700, 500, null);
+		c.println("Look Outside!");
 		Thread.sleep(2000);
-		System.out.println("What is your current weather condition? (sunny, cloudy, raining, snowing): ");
-		String weather = scanner.nextLine().toLowerCase();
+		c.println("What is your current weather condition? (sunny, cloudy, raining, snowing): ");
+		String weather = c.readLine().toLowerCase();
 
 		while (!weather.equals("sunny") && !weather.equals("cloudy") && !weather.equals("raining") && !weather.equals("snowing")) {
-			System.out.println("That wasnt an option :(");
+			c.clear();
+			c.println("That wasnt an option :(");
 			Thread.sleep(2000);
-			System.out.println("Please enter one of the following: sunny, cloudy, raining, snowing.");
-			weather = scanner.nextLine().toLowerCase();
+			c.println("Please enter one of the following: sunny, cloudy, raining, snowing.");
+			weather = c.readLine().toLowerCase();
 		}
 		return weather;
 	}
 
 	private static void drawImage(BufferedImage wizard2, int i, int j, int k, int l, Object object) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private static void clear() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public static int getUserInputTemperature() throws InterruptedException {
 		// Asks the user to input the current temperature in Celsius
-		Scanner scanner = new Scanner(System.in);
 
-		System.out.println("Noted!");
-		System.out.println(" ");
+		c.println("Noted!");
+		c.println(" ");
 		Thread.sleep(2000);
-		System.out.println("Question 2:");
-		System.out.println("-----------");
+		c.clear();
+		c.println("Question 2:");
+		c.println("-----------");
 		Thread.sleep(2000);
-		System.out.println("Either check online..");
+		c.println("Either check online..");
 		Thread.sleep(2000);
-		System.out.println("Or make an educated guess!");
+		c.println("Or make an educated guess!");
 		Thread.sleep(2000);
-		System.out.println("Enter the current temperature (in Celsius): ");
-		int temperature = scanner.nextInt();
+		c.clear();
+		c.drawImage (thermometer,  0, 0, 700, 500, null);
+		c.println("Enter the current temperature (in Celsius): ");
+		int temperature = c.readInt();
 
 		return temperature;
 	}
 
 	public static String getUserInputTimeOfDay() throws InterruptedException {
 		// Asks the user to input the time of day (morning, afternoon, night)
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.println("Got it!!");
-		System.out.println(" ");
+		c.println("Got it!!");
+		c.println(" ");
 		Thread.sleep(2000);
-		System.out.println("Question 3:");
-		System.out.println("-----------");
+		c.println("Question 3:");
+		c.println("-----------");
 		Thread.sleep(2000);
-		System.out.println("Please enter the time of day (morning, afternoon, night): ");
-		String timeOfDay = scanner.nextLine().toLowerCase();
+		c.clear();
+		c.drawImage (timeofday,  0, 0, 700, 500, null);
+		c.println("Please enter the time of day (morning, afternoon, night): ");
+		String timeOfDay = c.readLine().toLowerCase();
 
 		while (!timeOfDay.equals("morning") && !timeOfDay.equals("afternoon") && !timeOfDay.equals("night")) {
-			System.out.println("That wasnt an option :(");
+			c.println("That wasnt an option :(");
 			Thread.sleep(2000);
-			System.out.println("Please enter one of the following: morning, afternoon, night.");
-			timeOfDay = scanner.nextLine().toLowerCase();
+			c.println("Please enter one of the following: morning, afternoon, night.");
+			timeOfDay = c.readLine().toLowerCase();
 		}
 		return timeOfDay;
 	}
@@ -166,38 +245,60 @@ public class FinalProject {
 		String prediction = "Weather prediction for the next few hours: ";
 
 		// Sunny conditions
+		c.clear();
+		c.drawImage (veryhot,  0, 0, 700, 500, null);
 		if (weather.equals("sunny")) {
 			if (temperature > 25) {
 				prediction += "It will stay sunny and hot! Perfect for a beach day or a hot day outside!";
+				c.clear();
+				c.drawImage (veryhot,  0, 0, 700, 500, null);
 			} else if (temperature >= 15) {
+				c.clear();
+				c.drawImage (sunnycloud,  0, 0, 700, 500, null);
 				prediction += "It will stay sunny but a bit cooler. Enjoy the pleasant weather!";
 			} else {
+				c.clear();
+				c.drawImage (sunnycloud,  0, 0, 700, 500, null);
 				prediction += "It will still be sunny, but might want a light jacket.";
 			}
 		}
 		// Cloudy conditions
 		else if (weather.equals("cloudy")) {
+			c.clear();
+			c.drawImage (sunnycloud,  0, 0, 700, 500, null);
 			if (temperature > 20) {
 				prediction += "It will stay cloudy, but warm. It could be a nice day for a walk!";
 			} else {
+				c.clear();
+				c.drawImage (clouds,  0, 0, 700, 500, null);
 				prediction += "It will stay cloudy and chilly. A good day to stay inside!";
 			}
 		}
 		// Raining conditions
 		else if (weather.equals("raining")) {
 			if (timeOfDay.equals("morning")) {
+				c.clear();
+				c.drawImage (rain,  0, 0, 700, 500, null);
 				prediction += "The rain will likely continue through the morning. Don't forget your umbrella!";
 			} else if (timeOfDay.equals("afternoon")) {
+				c.clear();
+				c.drawImage (rain,  0, 0, 700, 500, null);
 				prediction += "The rain will ease off in the afternoon, but expect some showers later.";
 			} else {
+				c.clear();
+				c.drawImage (rain,  0, 0, 700, 500, null);
 				prediction += "The rain will continue through the night, so stay indoors if you can!";
 			}
 		}
 		// Snowing conditions
 		else if (weather.equals("snowing")) {
 			if (temperature < 0) {
+				c.clear();
+				c.drawImage (snowman,  0, 0, 700, 500, null);
 				prediction += "The snow will continue. It's a great time for snowball fights or building a snowman!";
 			} else {
+				c.clear();
+				c.drawImage (muddy,  0, 0, 700, 500, null);
 				prediction += "The snow will likely turn to slush. Be careful on the roads!";
 			}
 		}
@@ -207,7 +308,7 @@ public class FinalProject {
 
 	public static void printPrediction(String prediction) {
 		// Prints the weather prediction to the user
-		System.out.println(prediction);
+		c.println(prediction);
 
 
 	}
